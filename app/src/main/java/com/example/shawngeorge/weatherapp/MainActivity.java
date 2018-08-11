905ed097ac630062906a5bbac2086f5ffb12b7dc
Shawn George <sjgeorg2@ncsu.edu>
2018-08-11 11:28:09 -0400

    initial commit

---
+++ app/src/main/java/com/example/shawngeorge/weatherapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.shawngeorge.weatherapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView weatherDisplay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** References TextView from XML*/
+        weatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
+
+        /** String array of random weather data to display on app for now */
+        String[] mockWeatherData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Thunderstorms - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Meatballs - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
+        };
+
+        /** Appends array of mock data to text view */
+        for(String dayData : mockWeatherData)
+        {
+            weatherDisplay.append(dayData + "\n\n\n");
+        }
+    }
+
+}
