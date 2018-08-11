d80f4b33e7056fefe4c9ff11fae46c723ab023d3
Shawn George <sjgeorg2@ncsu.edu>
2018-08-11 12:57:22 -0400

    updated weather api

--- app/src/main/java/com/example/shawngeorge/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/shawngeorge/weatherapp/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.shawngeorge.weatherapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import data.WeatherPreferences;
+import utilities.NetworkUtils;
+import utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,29 +22,52 @@ public class MainActivity extends AppCompatActivity {
         /** References TextView from XML*/
         weatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
-        /** String array of random weather data to display on app for now */
-        String[] mockWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
-
-        /** Appends array of mock data to text view */
-        for(String dayData : mockWeatherData)
-        {
-            weatherDisplay.append(dayData + "\n\n\n");
-        }
+        loadWeatherData();
     }
 
+    private void loadWeatherData()
+    {
+        String location;
+        location = WeatherPreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected String[] doInBackground(String... params) {
+            if(params.length == 0)
+            {
+                return null;
+            }
+            String location = params[0];
+            URL weatherURL = NetworkUtils.buildURL(location);
+
+            try
+            {
+                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherURL);
+                String[] simpleWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleWeatherData;
+            } catch(Exception e)
+            {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData)
+        {
+            if(weatherData != null)
+            {
+                for(String data:weatherData)
+                {
+                    weatherDisplay.append(data + "\n\n\n");
+                }
+            }
+        }
+
+
+
+    }
 }
